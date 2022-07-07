9f44c8b74859694bce225c477360e9de2452c69d
Peter Seprus <ppseprus@users.noreply.github.com>
2022-07-07 11:20:40 +0200

    Separate logical blocks
    This improves readability / decreases cognitive complexity for the reader or reviewer.

--- maximum_minimum.py
+++ maximum_minimum.py
@@ -14,13 +14,16 @@ smallest = None
 
 while True:
     sval = input("Enter a number: ")
+    
     if sval == 'done':
         break
+    
     try:
         fval = float(sval)
     except:
         print("Invalid input")
         continue
+    
     if largest is None:
         largest = fval
     elif smallest is None:
