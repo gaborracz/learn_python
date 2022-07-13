2ddb4ac8082820f64561d7a645285ae47eb849b2
Gabor Racz <racz.gabor@telekom.hu>
2022-07-13 11:13:26 +0200

    adding comments

--- assignment7.2_string_manipulation.py
+++ assignment7.2_string_manipulation.py
@@ -1,3 +1,12 @@
+# 7.2 Write a program that prompts for a file name, then opens that file and reads through the file, 
+# looking for lines of the form:
+# X-DSPAM-Confidence:    0.8475
+# Count these lines and extract the floating point values from each of the lines and compute 
+# the average of those values and produce an output as shown below. Do not use the sum() 
+# function or a variable named sum in your solution.
+# You can download the sample data at http://www.py4e.com/code3/mbox-short.txt when you are 
+# testing below enter mbox-short.txt as the file name.
+
 fileName = input("Enter file name: ")
 
 fileHandler = open(fileName)
@@ -18,4 +27,3 @@ for line in fileHandler :
     lineCounter = lineCounter + 1
 
 print("Average spam confidence: %r" % (numbers / lineCounter))
-
